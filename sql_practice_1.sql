095bd2a27b3a6ad361f9e14b19850ee62f772bee
Azim Akmatov <49513936+akmatoff@users.noreply.github.com>
2021-10-23 13:18:26 +0600

    Update sql_practice_1.sql

--- sql_practice_1.sql
+++ sql_practice_1.sql
@@ -1,7 +1,7 @@
 -- Task 1
 
 CREATE TABLE employee (
-  id INTEGER auto_increment PRIMARY KEY,
+  id integer PRIMARY KEY AUTOINCREMENT,
   Fname varchar(60),
   Lname varchar(60),
   BirthDate date,
@@ -12,7 +12,7 @@ CREATE TABLE employee (
 );
 
 CREATE TABLE Departments (
-    id INTEGER auto_increment PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     Name varchar(60)
 );
 
