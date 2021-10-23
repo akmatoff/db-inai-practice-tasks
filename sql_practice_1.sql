f6575caf2345e8ccfaef990d9c435278d69b6bc1
Azim Akmatov <49513936+akmatoff@users.noreply.github.com>
2021-10-23 13:25:25 +0600

    Update sql_practice_1.sql

--- sql_practice_1.sql
+++ sql_practice_1.sql
@@ -22,9 +22,9 @@ INSERT INTO Departments(Name) VALUES ('Wow'), ('HEY'), ('NO'), ('LOL');
 INSERT INTO employee(Fname, LName, Salary, BirthDate, DepartmentID, Gender) VALUES ('Zhumabekov', 'Baidolot', 80000, '25/02/1987', 1, 'male');
 INSERT INTO employee(Fname, LName, Salary, BirthDate, DepartmentID, Gender) VALUES ('Washington', 'George', 120000, '14/06/1995', 2, 'male');
 INSERT INTO employee(Fname, LName, Salary, BirthDate, DepartmentID, Gender) VALUES ('Little', 'Stuart', 25000, '10/10/1992', 3, 'male');
-INSERT INTO employee(Fname, LName, Salary, BirthDate, DepartmentID, Gender) VALUES ('Big', 'Smoke', 12000, '24/12/1991', 'male');
-INSERT INTO employee(Fname, LName, Salary, BirthDate, DepartmentID, Gender) VALUES ('Smart', 'Phone', 50000, '12/09/1989', 'male');
-INSERT INTO employee(Fname, LName, Salary, BirthDate, DepartmentID, Gender) VALUES ('Smartie', 'Phonie', 10000, '16/11/1990', 'female');
+INSERT INTO employee(Fname, LName, Salary, BirthDate, DepartmentID, Gender) VALUES ('Big', 'Smoke', 12000, '24/12/1991', 4, 'male');
+INSERT INTO employee(Fname, LName, Salary, BirthDate, DepartmentID, Gender) VALUES ('Smart', 'Phone', 50000, '12/09/1989', 4, 'male');
+INSERT INTO employee(Fname, LName, Salary, BirthDate, DepartmentID, Gender) VALUES ('Smartie', 'Phonie', 10000, '16/11/1990', 2, 'female');
 
 -- Task 3
 SELECT Fname + Lname AS FIO FROM employee;
